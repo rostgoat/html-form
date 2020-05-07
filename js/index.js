f0237c690ed0daacc6ec9fb4dc713fcd1a7bc779
Rost Myshkin <rostmyshkin@gmail.com>
2020-05-07 02:14:24 -0700

    fnished form submission

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ answers.forEach(function(answer, index) {
 
     var removeBtn = document.createElement('button');
     removeBtn.setAttribute('class', 'form-bottom__item-remove-btn');
-    removeBtn.setAttribute('onClick', `removeAnswer("${index}")`);
+    removeBtn.setAttribute('onClick', `removeAnswer(e, "${index}")`);
     removeBtn.appendChild(document.createTextNode('x'));
 
     item.appendChild(image);
@@ -48,11 +48,16 @@ answers.forEach(function(answer, index) {
     answersList.appendChild(item);
 });
 
+function addAnswer(title) {
+    answers.push(title);
+}
+
 /**
  * Remove answer from array
  * @param {Number} itemToRemove 
  */
-function removeAnswer(itemToRemove) {
+function removeAnswer(e, itemToRemove) {
+    e.preventDefault();
     var item = document.getElementById(itemToRemove);
     answersList.removeChild(item);
 }
@@ -61,6 +66,29 @@ function removeAnswer(itemToRemove) {
  * Prevent form from submitting involuntarily
  * @param {Event} e 
  */
-function submitForm(e) {
-    e.preventDefault();
+function submitForm() {
+    var shortAnswer = document.getElementById('short_answer').value;
+    var longAnswer = document.getElementById('long_answer').value;
+    var multipleChoiceAnswer;
+
+    var array = Array.prototype.slice.call(answersList.querySelectorAll('.form-bottom__item'));
+
+    array.forEach(function(item) {
+        var current = item.querySelector('.form-bottom__item-radio-btn')
+        if (current.checked) {
+            multipleChoiceAnswer = current.parentElement.querySelector('.form-bottom__item-text').textContent;
+        }
+    })
+
+    console.log('shortAnswer', shortAnswer)
+    console.log('longAnswer', longAnswer)
+    console.log('multipleChoiceAnswer', multipleChoiceAnswer)
+
+    var dataToSubmit = {
+        shortAnswer: shortAnswer,
+        longAnswer: longAnswer,
+        multipleChoiceAnswer: multipleChoiceAnswer
+    }
+
+    document.getElementById('questionForm').submit();
 }
